9f587e84256324afb31c4cb17576ee8fd8e93e47
Sam Chillcott <samchillcott@gmail.com>
2021-06-28 15:22:52 +0100

    add button with hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LoginButton from './components/LoginButton';
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginButton></LoginButton>
     </div>
   );
 }
